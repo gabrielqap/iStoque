81ad9d876fcfc46e03f09b1373fa433b3ebccff8
gabrielqap <gabrielqap@hotmail.com>
2019-06-13 19:57:44 -0300

    Iniciando classes de dominio...

---
+++ iStoque/iStoqueSystem/src/java/domain/iStoque/Functionary.java
@@ -0,0 +1,5 @@
+package iStoque;
+
+public class Functionary {
+
+}
---
+++ iStoque/iStoqueSystem/src/java/domain/iStoque/Product.java
@@ -0,0 +1,42 @@
+package iStoque;
+
+public class Product {
+	private int id;
+	private String productName;
+	private String category;
+	private float value;
+	private int quantity;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public float getValue() {
+		return value;
+	}
+	public void setValue(float value) {
+		this.value = value;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
